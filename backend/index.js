254d4fe7b4b8a403816587e73379c6b36413e9fd
Edmilson Silva <ed1000sons@gmail.com>
2022-08-11 14:24:46 +0000

    feat: adicionado morgan, helmet, compression  para melhorar a segurança e a estrutura do código

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
+const compression = require("compression");
+const { default: helmet } = require("helmet");
 const app = express();
+
+app.use(morgan("dev"));
+app.use(compression());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
+app.disable('x-powered-by');
 
 app.get("/", (_req, res) => {
   res.send("Hello, World!");
